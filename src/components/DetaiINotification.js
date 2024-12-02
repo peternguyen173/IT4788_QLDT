aee463ebd7c62e8073652529bb93cf908ef52c6a
hiep957 <hicharmie1@gmail.com>
2024-12-02 21:22:43 +0700

    merge khó vãi

--- src/components/DetaiINotification.js
+++ src/components/DetaiINotification.js
@@ -4,7 +4,8 @@ import { Text } from "react-native"
 const DetailNotification = (props) => {
     console.log(props)
     return (
-        <>
+        <>  
+            <Text>dsada</Text>
             <Text>Detail</Text>
             <Text>222</Text>
         </>
